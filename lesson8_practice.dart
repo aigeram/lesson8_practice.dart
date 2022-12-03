1bc9643a512b85ebb919a22ceeebd969f608c740
aigeram <118278771+aigeram@users.noreply.github.com>
2022-12-03 16:58:31 +0600

    lesson8_practice.dart

---
+++ lesson8_practice.dart
@@ -0,0 +1,136 @@
+
+
+import '../lesson7/lesson7_practice.dart/lesson7_practice.dart';
+
+void main(){
+taska();
+taskb();
+task1();
+task2();
+task();
+task5();
+
+
+int a=2;
+int b=2;
+int q=(a+b);
+print(q);
+
+//trite
+ 
+int w=5;
+int e=10;
+int r=(w-e);
+print(r);
+
+
+//trite
+
+int h=16;
+int l=3;
+int p=(h*l);
+print(p);
+
+//trite
+
+int s=18;
+int x=2;
+double o=(s/x);
+print(o);
+
+
+//trite
+
+int m=12;
+int t=15;
+ var y=(m>t);
+ print(y);
+
+ // trite
+ int o1=12;
+ int r1=15;
+ bool y1=(o1<r1);
+ print(y1);
+
+ //trite
+ int f2=12;
+ int t2=15;
+ bool y2=(f2<=t2);
+ print(y2);
+
+ //trite
+ int f3=12;
+ int t3=15;
+ bool y3=(f3>=t3);
+ print(y3);
+
+ //trite
+ int f4=12;
+ int t4=15;
+ bool y4=(f4==t4);
+ print(y4);
+
+}
+
+  
+
+
+task1(){
+  int mul(int a,int b){
+  int q=(a+b);
+  return q;
+}
+}
+
+task2(){
+  int mul1 (int k,inty){
+    int k=5;
+    int y=10;
+  int o=(k-y);
+  return o;
+}
+}
+task(){
+int sum(int m,int n){
+  int m=16;
+  int n=3;
+  int h=(m*n);
+  return h;
+}
+}
+
+task3(){
+  int mul2(int q,int f){
+    int q=18;
+    int f=2;
+    double k=(18/2);
+   return (q);
+  }
+}
+
+task5(){
+  dynamic b=[1,2,3,4,5,6];
+  if(6<=15){
+    print(true);
+  }else{
+    print(false);
+  }
+}
+
+taska(){
+  dynamic a=[1,2,3,4,5];
+  if(a==1||a==2||a==3){
+    print(true);
+  }else{
+    print(false);
+  }
+
+}
+taskb(){
+  String nb=('a,b,c,d');
+  if(nb=='a'){
+    print(true);
+  }else{
+    print(false);
+  }
+}
